50dd93bd869ab6a0783b59ed5225322f130f2127
RealElBarto <143960925+RealElBarto@users.noreply.github.com>
2023-10-22 17:17:21 +0200

    script: updated script file

--- script.py
+++ script.py
@@ -1,13 +1,10 @@
-# Script is a class to pares and serialize bitcoin Scripts
 from format_converter import Converter
 from helper_functions import read_varint, encode_varint
-from op import ( OP_CODE_FUNCTIONS, OP_CODE_NAMES)
-from logging import getLogger
+from op import OP_CODE_NAMES
 
-
-LOGGER = getLogger(__name__)
 
 converter = Converter()
+
 
 def p2pkh_script(hash160: str) -> "Script":
     
@@ -16,138 +13,76 @@ def p2pkh_script(hash160: str) -> "Script":
 
 class Script:
 
-    def __init__(self, cmds=None):
-        if cmds is None:
-            self.cmds = []
+    def __init__(self, commands=None):
+        if commands is None:
+            self.commands = []
         else:
-            self.cmds = cmds
+            self.commands = commands
 
-    def __repr__(self):
+
+    def __repr__(self) -> str:
         result = []
-        for cmd in self.cmds:
-            if type(cmd) == int:
-                if OP_CODE_NAMES.get(cmd):
-                    name = OP_CODE_NAMES.get(cmd)
+        for command in self.commands:
+            if type(command) == int:
+                if OP_CODE_NAMES.get(command):
+                    name = OP_CODE_NAMES.get(command)
                 else:
-                    name = 'OP_[{}]'.format(cmd)
+                    name = 'OP_[{}]'.format(command)
                 result.append(name)
             else:
-                result.append(cmd.hex())
+                result.append(command.hex())
         return ' '.join(result)
 
+
     @classmethod
-    def parse(cls, s):
-        # get the length of the entire field
-        length = read_varint(s)
-        # initialize the cmds array
-        cmds = []
-        # initialize the number of bytes we've read to 0
+    def parse(cls, stream: bytes) -> "Script":
+        length = read_varint(stream)
+        commands = []
         count = 0
-        # loop until we've read length bytes
         while count < length:
-            # get the current byte
-            current = s.read(1)
-            # increment the bytes we've read
+            current = stream.read(1)
             count += 1
-            # convert the current byte to an integer
             current_byte = current[0]
-            # if the current byte is between 1 and 75 inclusive
             if current_byte >= 1 and current_byte <= 75:
-                # we have an cmd set n to be the current byte
                 n = current_byte
-                # add the next n bytes as an cmd
-                cmds.append(s.read(n))
-                # increase the count by n
+                commands.append(stream.read(n))
                 count += n
             elif current_byte == 76:
-                # op_pushdata1
-                data_length = converter.little_endian_to_int(s.read(1))
-                cmds.append(s.read(data_length))
+                data_length = converter.little_endian_to_int(stream.read(1))
+                commands.append(stream.read(data_length))
                 count += data_length + 1
             elif current_byte == 77:
-                # op_pushdata2
-                data_length = converter.little_endian_to_int(s.read(2))
-                cmds.append(s.read(data_length))
+                data_length = converter.little_endian_to_int(stream.read(2))
+                commands.append(stream.read(data_length))
                 count += data_length + 2
             else:
-                # we have an opcode. set the current byte to op_code
                 op_code = current_byte
-                # add the op_code to the list of cmds
-                cmds.append(op_code)
+                commands.append(op_code)
         if count != length:
             raise SyntaxError('parsing script failed')
-        return cls(cmds)
+        
+        return cls(commands)
     
 
-    def serialize(self):
-        # initialize what we'll send back
-        result = b''
-        # go through each cmd
-        for cmd in self.cmds:
-            # if the cmd is an integer, it's an opcode
-            if type(cmd) == int:
-                # turn the cmd into a single byte integer using int_to_little_endian
-                result += converter.int_to_little_endian(cmd, 1)
+    def serialize(self) -> str:
+        script = b''
+        for command in self.commands:
+            if type(command) == int:
+                script += converter.int_to_little_endian(command, 1)
             else:
-                # otherwise, this is an element
-                # get the length in bytes
-                length = len(cmd)
-                # for large lengths, we have to use a pushdata opcode
+                length = len(command)
                 if length < 75:
-                    # turn the length into a single byte integer
-                    result += converter.int_to_little_endian(length, 1)
+                    script += converter.int_to_little_endian(length, 1)
                 elif length > 75 and length < 0x100:
-                    # 76 is pushdata1
-                    result += converter.int_to_little_endian(76, 1)
-                    result += converter.int_to_little_endian(length, 1)
+                    script += converter.int_to_little_endian(76, 1)
+                    script += converter.int_to_little_endian(length, 1)
                 elif length >= 0x100 and length <= 520:
-                    # 77 is pushdata2
-                    result += converter.int_to_little_endian(77, 1)
-                    result += converter.int_to_little_endian(length, 2)
+                    script += converter.int_to_little_endian(77, 1)
+                    script += converter.int_to_little_endian(length, 2)
                 else:
-                    raise ValueError('too long an cmd')
-                result += cmd
-        total = len(result)
-    
-        return encode_varint(total) + result
+                    raise ValueError('too long an command')
+                script += command
 
+        total = len(script)
 
-    #def serialize(self):
-    #    # get the raw serialization (no prepended length)
-    #    result = self.raw_serialize()
-    #    # get the length of the whole thing
-    #    total = len(result)
-    #    # encode_varint the total length of the result and prepend
-    #    return encode_varint(total) + result
-
-    def evaluate(self, z):
-        cmds = self.cmds[:]
-        stack = []
-        altstack = []
-        while len(cmds) > 0:
-            cmd = cmds.pop(0)
-            if type(cmd) == int:
-                operation = OP_CODE_FUNCTIONS[cmd]
-                if cmd in (99, 100):
-                    if not operation(stack, cmds):
-                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
-                        return False
-                elif cmd in (107, 108):
-                    if not operation(stack, altstack):
-                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
-                        return False
-                elif cmd in (172, 173, 174, 175):
-                    if not operation(stack, z):
-                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
-                        return False
-                else:
-                    if not operation(stack):
-                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
-                        return False
-            else:
-                stack.append(cmd)
-        if len(stack) == 0:
-            return False  
-        if stack.pop() == b'':
-            return False  
-        return True  
+        return encode_varint(total) + script

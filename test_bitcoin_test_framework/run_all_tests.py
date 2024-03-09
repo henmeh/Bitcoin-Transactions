f381fa04c4ad46e0d3ade0bd1f281d59842c1557
henmeh <143960925+henmeh@users.noreply.github.com>
2024-03-09 19:14:37 +0100

    added test_p2wpkh.py

--- test_bitcoin_test_framework/run_all_tests.py
+++ test_bitcoin_test_framework/run_all_tests.py
@@ -3,5 +3,14 @@ import test_p2ms
 
 
 if __name__ == "__main__":
-    test_p2pk.P2PKTest().main()
-    test_p2ms.P2PKTest().main()
+    try:
+        test_p2pk.P2PKTest().main()
+    except Exception as e:
+        print(f"Error in P2PKTest: {e}")
+
+    print("Hallo")
+
+    #try:
+    #    test_p2ms.P2MSTest().main()
+    #except Exception as e:
+    #    print(f"Error in P2MSTest: {e}")
